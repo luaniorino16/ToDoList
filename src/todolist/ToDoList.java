30476bb4f6871a31bc230762ccc84f156e782481
Luan <luan.iorino@aluno.senai.br>
2026-08-20 09:16:46 -0300

    Alterações aula 20/08

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -12,17 +12,87 @@ import java.util.Scanner;
  */
 public class ToDoList {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        //teste
-        String [] Tarefas = new String[10]; 
-       int quantidade = 0;
-       int opcao;
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
        
+        String[] tarefas = new String[10];
+        boolean[] concluidas = new boolean[10];
        
-       do { 
- 
-       } while(opcao != 0 )
+        int opcao = 0;
+       
+        while(opcao != 5) {
+            System.out.println("======LISTA DE TAREFAS======");
+            System.out.println("1- Adicionar tarefa");
+            System.out.println("2- Listar tarefas");
+            System.out.println("3- Concluir tarefas");
+            System.out.println("4- Excluir tarefa");
+            System.out.println("5- Sair");
+           
+            System.out.println("Escolha uma opcao: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+           
+            switch(opcao){
+                case 1:
+                    int posicaoLivre = -1;
+                   
+                    for(int i = 0; i < tarefas.length; i++){
+                        if(tarefas[i] == null){
+                            posicaoLivre = i;
+                            break;
+                        }
+                    }
+                   
+                    if (posicaoLivre == -1){
+                        System.out.println("A lista de tarefas está cheia!");
+                    }else{
+                        System.out.println("Digite a tarefa: ");
+                        tarefas[posicaoLivre] = scanner.nextLine();
+                        concluidas[posicaoLivre] = false;
+                        System.out.println("Tarefa adicionada com sucesso!");
+                    }
+                   
+                    break;
+                   
+                case 2:
+                    System.out.println("======MINHAS TAREFAS======");
+                   
+                    boolean existeTarefa = false;
+                   
+                    for(int i = 0; i < tarefas.length; i++){
+                        if(tarefas[i] != null){
+                            existeTarefa = true;
+                           
+                            if (concluidas[i] == true){
+                                System.out.println((i + 1) + " - [x] " + tarefas[i]);
+                            }else{
+                                System.out.println((i + 1) + " - [ ] " + tarefas[i]);
+                            }
+                        }
+                    }
+                   
+                    if (existeTarefa == false){
+                        System.out.println("Nenuma tarefa cadastrada!");
+                    }
+                   
+                    break;
+                   
+                case 3:
+                    System.out.println("Digite o número da tarefa que deseja concluir: ");
+                    int numeroConcluir = scanner.nextInt();
+                   
+                    int indiceConcluir = numeroConcluir - 1;
+                   
+                    if (indiceConcluir >= 0 && indiceConcluir < tarefas.length && tarefas[indiceConcluir] != null){
+                       
+                        concluidas[indiceConcluir] = true;
+                       
+                        System.out.println("Tarefa concluida com sucesso!");
+                    }else{
+                        System.out.println("Tarefa inválida");
+                    }
+            }
+        }
+    }
+   
+}
